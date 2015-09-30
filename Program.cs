1e229a2c8c3e627594bc00cb6b6731209f8963ab
hide_m <hide_m@ym.tose.co.jp>
2015-09-30 21:46:12 +0900

    P186 ベースのプログラム。

--- Program.cs
+++ Program.cs
@@ -10,6 +10,57 @@ namespace ch06_01
 	{
 		static void Main(string[] args)
 		{
+			Turtle arthurTheTurtle =
+				new Turtle { PlatformWidth = 10.0, PlatformHeight = 10.0, MotorSpeed = 5.0 };
+
+			ShowPosition(arthurTheTurtle);
+
+			// 前進。
+			arthurTheTurtle.LeftMotorState = MotorState.Running;
+			arthurTheTurtle.RightMotorState = MotorState.Running;
+
+			// 2秒間。
+			arthurTheTurtle.RunFor(2.0);
+			ShowPosition(arthurTheTurtle);
+
+			// 時計回りに少し回転。
+			arthurTheTurtle.RightMotorState = MotorState.Reversed;
+
+			// PI/2秒。
+			arthurTheTurtle.RunFor(Math.PI / 2.0);
+			ShowPosition(arthurTheTurtle);
+
+			// 後退。
+			arthurTheTurtle.LeftMotorState = MotorState.Reversed;
+			arthurTheTurtle.RightMotorState = MotorState.Reversed;
+
+			// 5秒間。
+			arthurTheTurtle.RunFor(5);
+			ShowPosition(arthurTheTurtle);
+
+			// 反時計回りに回転。
+			arthurTheTurtle.RightMotorState = MotorState.Running;
+
+			// PI/4秒間で45度。
+			arthurTheTurtle.RunFor(Math.PI / 4.0);
+			ShowPosition(arthurTheTurtle);
+
+			// 少し後退。
+			arthurTheTurtle.RightMotorState = MotorState.Reversed;
+			arthurTheTurtle.LeftMotorState = MotorState.Reversed;
+			arthurTheTurtle.RunFor(Math.Cos(Math.PI / 4.0));
+
+			ShowPosition(arthurTheTurtle);
+
+			Console.ReadKey();
+		}
+
+		private static void ShowPosition(Turtle arthurTheTurtle)
+		{
+			Console.WriteLine(
+				"Arthurの位置は({0}),方向は{1:0.00}ラジアンです。",
+				arthurTheTurtle.CurrentPosition,
+				arthurTheTurtle.CurrentOrientation);
 		}
 	}
 }
---
+++ Turtle.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows;
+
+namespace ch06_01
+{
+	class Turtle
+	{
+		// 機体の幅。
+		public double PlatformWidth
+		{
+			get;
+			set;
+		}
+
+		// 機体の高さ。
+		public double PlatformHeight
+		{
+			get;
+			set;
+		}
+
+		// モーターのスピード。単位はメートル毎秒。
+		public double MotorSpeed
+		{
+			get;
+			set;
+		}
+
+		// 左モーターの状態。
+		public MotorState LeftMotorState
+		{
+			get;
+			set;
+		}
+
+		// 右モーターの状態。
+		public MotorState RightMotorState
+		{
+			get;
+			set;
+		}
+
+		// タートルの現在位置。
+		public Point CurrentPosition
+		{
+			get;
+			private set;
+		}
+
+		// タートルの現在の方向。
+		public double CurrentOrientation
+		{
+			get;
+			private set;
+		}
+
+		// 指定した時間だけタートルを走行させます。
+		public void RunFor(double duration)
+		{
+			if (LeftMotorState == MotorState.Stopped &&
+				RightMotorState == MotorState.Stopped)
+			{
+				// 完全に止まっている場合は何もしません。
+				return;
+			}
+
+			// モーターが同じ方向に動いている場合には直進します。
+			if ((LeftMotorState == MotorState.Running &&
+				RightMotorState == MotorState.Running) ||
+				(LeftMotorState == MotorState.Reversed &&
+				RightMotorState == MotorState.Reversed))
+			{
+				Drive(duration);
+				return;
+			}
+
+			// モーターが反対方向に動いている場合には回転します。
+			if ((LeftMotorState == MotorState.Running &&
+				RightMotorState == MotorState.Reversed) ||
+				(LeftMotorState == MotorState.Reversed &&
+				RightMotorState == MotorState.Running))
+			{
+				Rotate(duration);
+				return;
+			}
+		}
+
+		// 回転。
+		private void Rotate(double duration)
+		{
+			// 旋回の総円周(circumference).
+			double circum = Math.PI * PlatformWidth;
+
+			// 総移動距離。
+			double d = duration * MotorSpeed;
+			if (LeftMotorState == MotorState.Reversed)
+			{
+				// モーターが逆回転している場合は逆方向に回転します。
+				d *= -1.0;
+			}
+
+			// 1周のうちで回転した割合。
+			double proportionOfWholeCircle = d / circum;
+
+			// 回転適用。
+			CurrentOrientation =
+				CurrentOrientation + (Math.PI * 2.0 * proportionOfWholeCircle);
+		}
+
+		// 移動。
+		private void Drive(double duration)
+		{
+			// 総移動距離。
+			double d = duration * MotorSpeed;
+			if (LeftMotorState == MotorState.Reversed)
+			{
+				// モーターが逆回転している場合は逆方向に移動します。
+				d *= -1.0;
+			}
+
+			// 移動量計算。
+			double deltaX = d * Math.Sin(CurrentOrientation);
+			double deltaY = d * Math.Cos(CurrentOrientation);
+
+			// 座標更新。
+			CurrentPosition = new Point(CurrentPosition.X + deltaX,
+				CurrentPosition.Y + deltaY);
+		}
+	}
+	// モーターの現在の状態。
+	enum MotorState
+	{
+		Stopped,
+		Running,
+		Reversed,
+	}
+}
